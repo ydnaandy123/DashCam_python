cc74cd859f9b6523eaf1c5d953f69e954b7e22a8
Andy <ydnaandy123@gmail.com>
2016-06-24 10:47:06 +0800

    VSplusVM

--- playground/basic_syntax_test.py
+++ playground/basic_syntax_test.py
@@ -1,3 +1,6 @@
+import numpy
+import math
+
 print ('yolo!')
 b = \
 '''
@@ -13,3 +16,29 @@ a = b = c = 1
 print(a, b, c)
 a = 2
 print(a, b, c)
+
+print (r'hiiiir\niiiii')
+
+
+def printme( str ):
+   "This prints a passed string into this function"
+   print str
+   return
+
+printme('yo')
+
+# Function definition is here
+def printinfo( arg1, *vartuple ):
+   "This prints a variable passed arguments"
+   print "Output is: "
+   arg1 = math.ceil(arg1);
+   print math.ceil(arg1)
+   #for var in vartuple:
+   #   print var;
+   return;
+
+# Now you can call printinfo function
+x = 10.123;
+printinfo( x );
+print (x);
+printinfo( 70, 60, 50 )
